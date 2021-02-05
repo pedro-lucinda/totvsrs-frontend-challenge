2bc57c2a6191bad5399e2bc6e79adb54f9f3f590
pedro-lucinda <pedrolucinda2020@gmail.com>
2021-02-05 16:30:06 -0300

    Login Page -- working

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo/logo-totvs-rs.png";
 import "./style.scss";
 //hook
 import { useForm } from "../../hooks/useForm";
 //router
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
 const Login = () => {
+  const [users, setUsers] = useState(() => {
+    const usersInLocalStorage = localStorage.getItem("users");
+    if (usersInLocalStorage) {
+      return JSON.parse(usersInLocalStorage);
+    } else {
+      return [];
+    }
+  });
+
   const [form, onChangeInput] = useForm({
     email: "",
     password: "",
   });
 
+  const history = useHistory();
+
   function handleLogin(e) {
     e.preventDefault();
+
+    const email = form.email;
+    const password = form.password;
+
+    const isRegistered = users?.filter(
+      (user) => user.email === email && user.password === password
+    );
+
+    console.log("isRegistered", isRegistered);
+
+    if (isRegistered.length > 0) {
+      history.push("/home");
+    } else {
+      alert("User not registered, please signup.");
+    }
   }
 
   return (
@@ -27,6 +53,8 @@ const Login = () => {
           onChange={onChangeInput}
           name={"email"}
           placeholder="email"
+          type="email"
+          required
         />
 
         <label> password </label>
@@ -35,6 +63,8 @@ const Login = () => {
           onChange={onChangeInput}
           name={"password"}
           placeholder="password"
+          type="password"
+          required
         />
 
         <button type="submit"> login </button>
